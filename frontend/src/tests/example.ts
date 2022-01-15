8624bfdf7dcf280487e4052dc7697c985e2df604
kfron <jakubfron99@gmail.com>
2022-01-15 16:41:03 +0100

    Tests: Example test

--- frontend/src/tests/example.ts
+++ frontend/src/tests/example.ts
@@ -1,6 +1,13 @@
+import { Position } from 'nativescript-google-maps-sdk';
+import 'reflect-metadata';
+import { DistanceFromPipe } from '../app/shared/pipes/distance-from-user.pipe';
+
 // A sample Jasmine test
-describe('A suite', function() {
-	it('contains spec with an expectation', function() {
-		expect(true).toBe(true);
+describe('A suite', function () {
+	const pos1 = Position.positionFromLatLng(51, 20);
+	const pos2 = Position.positionFromLatLng(51, 20);
+	const pipe = new DistanceFromPipe();
+	it('contains spec with an expectation', function () {
+		expect(pipe.transform(pos1, pos2)).toEqual('0 meters away');
 	});
 });

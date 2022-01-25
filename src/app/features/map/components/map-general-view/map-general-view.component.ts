d0d8a98f02204101d99cad77fc8c1afe493ed079
kfron <jakubfron99@gmail.com>
2022-01-25 19:46:20 +0100

    Add apk file

--- src/app/features/map/components/map-general-view/map-general-view.component.ts
+++ src/app/features/map/components/map-general-view/map-general-view.component.ts
@@ -263,6 +263,7 @@ export class MapGeneralViewComponent implements OnInit, OnDestroy {
 				this.closestFindings = findings;
 				this.setupAdMarkers();
 			}));
+		this.clearPath();
 		this.updateSearchCircle();
 	}
 
